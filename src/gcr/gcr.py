0463655a1ce1795ad0593d4200dcb21b453c2891
Tom Faulkner <tomfaulkner@gmail.com>
2021-01-25 19:40:22 -0600

    add template, output

--- src/gcr/gcr.py
+++ src/gcr/gcr.py
@@ -1,32 +1,58 @@
 import csv
+import os
 from collections import defaultdict
+from contextlib import suppress
 from decimal import Decimal as D
 
+from jinja2 import Environment, PackageLoader, select_autoescape
 
-fields = ('Amount Num.', 'Date')
+env = Environment(
+    loader=PackageLoader("gcr", "templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
 
-def print_report():
+fields = ("amount", "date", "number")
+
+
+def load_template(name: str = "default.template") -> Environment:
+    return env.get_template(name)
+
+
+def rename_columns(row):
+    mapping = (("Amount Num.", "amount"), ("Date", "date"), ("Number", "number"))
+    for before, after in mapping:
+        row[after] = row[before]
+    return row
+
+
+def main():
+    prepare_environment()
     people = defaultdict(dict)
     with open("20-checking.csv") as f:
         reader = csv.DictReader(f)
         for r in reader:
             if is_giving_row(r):
-                print(f"{r['Date']}\t {r['Description']}\t {r['Amount Num.']}")
-                total = people[r['Description']].get('total', D('0'))
-                total += D(r['Amount Num.'].replace(',', ''))
-                people[r['Description']]['total'] = total
+                r = rename_columns(r)
+                total = people[r["Description"]].get("total", D("0"))
+                total += D(r["amount"].replace(",", ""))
+                people[r["Description"]]["total"] = total
                 try:
-                    people[r['Description']]['instances'].append({
-                        f: r[f] for f in r if f in fields
-                    })
+                    people[r["Description"]]["instances"].append(
+                        {f: r[f] for f in r if f in fields}
+                    )
                 except KeyError:
-                    people[r['Description']]['instances'] = []
-                    people[r['Description']]['instances'].append({
-                        f: r[f] for f in r if f in fields
-                    })
-    from pprint import pprint
-    pprint(people)
+                    people[r["Description"]]["instances"] = []
+                    people[r["Description"]]["instances"].append(
+                        {f: r[f] for f in r if f in fields}
+                    )
 
+    temp = load_template()
+    for person, values in people.items():
+        output = temp.render(
+            name=person, instances=values["instances"], total=values["total"]
+        )
+        with open(f'output/{person.replace(" ", "_")}.html', "w") as f:
+            f.write(output)
 
 
 def get_names():
@@ -44,5 +70,10 @@ def is_giving_row(row):
     return False
 
 
+def prepare_environment():
+    with suppress(FileExistsError):
+        os.mkdir("output")
+
+
 if __name__ == "__main__":
-    print_report()
+    main()
